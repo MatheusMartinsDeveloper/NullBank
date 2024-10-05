cc50a61619379b88eabb0383e1fdbb6b4b435ad6
MatheusMartinsDeveloper <matheus2005brr@gmail.com>
2024-10-05 17:58:07 -0300

    feat(homepage): Building Section Hero component

--- my-project/src/app/components/SectionHero.tsx
+++ my-project/src/app/components/SectionHero.tsx
@@ -1,19 +1,27 @@
-import Button from "./Button";
+import Image from "next/image";
+import Link from "next/link";
+import CardFront from "../../../public/images/card-front.svg";
+import CardBack from "../../../public/images/card-back.svg";
 
 export default function SectionHero() {
     return(
-        <section>
-            <div>
-                <div>
-                    <h1>Banco <span>100% digital</span> e <br /> com tarifas <span>nulas</span></h1>
-                    <p>O banco digital que oferece serviços financeiros sem <br />
+        <section className="flex justify-between w-full h-[93vh]">
+            <div className="flex justify-center items-center bg-backgroundColor w-full h-full">
+                <div className="flex flex-col items-start gap-5">
+                    <h1 className="text-grayColorExtra text-5xl leading-snug font-Poppins font-semibold">Banco <span className="text-primaryColorMain">100% digital</span> e <br /> com tarifas <span className="text-primaryColorMain">nulas</span></h1>
+                    <p className="text-grayColorDark text-base font-Poppins font-normal">O banco digital que oferece serviços financeiros sem <br />
                     complicações e tarifas. Mantenha suas finanças sob <br />
                     controle com segurança e agilidade. </p>
-                    <Button />
+                    <Link className="bg-gradient rounded-lg py-2 px-7 delay-75 transition-all hover:delay-75 hover:ease-in-out hover:scale-[110%]" href={`#`}>
+                        <span className="text-grayColorLight text-base uppercase font-Poppins font-medium">Baixe o app</span>
+                    </Link>
                 </div>
             </div>
-            <div>
-                <div></div>
+            <div className="relative flex justify-center items-center bg-primaryColorMain w-full h-full">
+                <div>
+                    <Image className="absolute top-56 left-36 z-50" src={CardFront} alt="Imagem Cartão" quality={100} />
+                    <Image className="absolute top-80 left-[25%] z-0" src={CardBack} alt="Imagem Cartão" quality={100} />
+                </div>
             </div>
         </section>
     );
