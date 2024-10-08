a1b3780928871dad66212e3542f38d8a89afd4e2
MatheusMartinsDeveloper <matheus2005brr@gmail.com>
2024-10-08 17:59:55 -0300

    feat(homepage): Add PrimeReact in Header component

--- my-project/src/app/components/Header.tsx
+++ my-project/src/app/components/Header.tsx
@@ -1,24 +1,52 @@
 import Link from "next/link";
-import { IoIosArrowDown } from "react-icons/io";
+import { Menubar } from "primereact/menubar";
 
 export default function Header() {
+    const items = [
+        {
+            label: "O NullBank",
+            items: [
+                {
+                    label: "Sobre",
+                },
+                {
+                    label: "Blog",
+                },
+                {
+                    label: "Comunidade",
+                },
+                {
+                    label: "Investidores",
+                }
+            ]
+        },
+        {
+            label: "Para você",
+            items: [
+                {
+                    label: "Pix",
+                },
+                {
+                    label: "Cartões",
+                },
+                {
+                    label: "App",
+                },
+                {
+                    label: "Seguros",
+                }
+            ]
+        },
+        {
+            label: "Perguntas"
+        }
+    ];
+
     return (
         <header className="flex justify-around items-center w-full">
             <div className="flex justify-center items-center gap-10 bg-backgroundColor py-6 w-[50%]">
                 <h1 className="text-4xl font-Poppins font-semibold bg-gradient bg-clip-text text-transparent">Nullbank</h1>
-                <nav>
-                    <ul className="flex items-center gap-10">
-                        <li>
-                            <Link className="flex items-center gap-5 text-grayColorExtra text-base font-Poppins font-normal" href={`#`}>O NullBank <IoIosArrowDown size={20} /></Link>
-                        </li>
-                        <li>
-                            <Link className="flex items-center gap-5 text-grayColorExtra text-base font-Poppins font-normal" href={`#`}>Para você <IoIosArrowDown size={20} /></Link>
-                        </li>
-                        <li>
-                            <Link className="text-grayColorExtra text-base font-Poppins font-normal" href={`#`}>Perguntas</Link>
-                        </li>
-                    </ul>
-                </nav>
+                <Menubar model={items} />
             </div>
             <div className="flex justify-center items-center bg-primaryColorMain py-6 w-[50%]">
                 <Link className="bg-grayColorLight rounded-lg py-2 px-7 delay-75 transition-all hover:delay-75 hover:ease-in-out hover:scale-[110%]" href={`#`}>
